d314d796e887e6a9e9218fe3a69083b50ff5e425
shrutika <sm77shruti@gmail.com>
2020-09-28 14:37:20 +0530

    Updates

---
+++ precache-manifest.703220634598b8a68131f080a0b1bc9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354debc5f62ef26cef50e532c0e57e33",
+    "url": "/News-app-Reactjs/index.html"
+  },
+  {
+    "revision": "872cda73bf70ee8211df",
+    "url": "/News-app-Reactjs/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fee8ddb578ae69689289",
+    "url": "/News-app-Reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "872cda73bf70ee8211df",
+    "url": "/News-app-Reactjs/static/js/2.941a2d10.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/News-app-Reactjs/static/js/2.941a2d10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee8ddb578ae69689289",
+    "url": "/News-app-Reactjs/static/js/main.9735c698.chunk.js"
+  },
+  {
+    "revision": "73e8193d59565820e914",
+    "url": "/News-app-Reactjs/static/js/runtime-main.dc223e27.js"
+  }
+]);
